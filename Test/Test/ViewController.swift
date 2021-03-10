18d0620eee87099e47bbd0b05b313a00c516ba29
thejabomma <theja@biocipher.in>
2021-03-10 10:40:32 +0530

    ic

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by apple on 10/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("some")
+    }
+
+
+}
+
